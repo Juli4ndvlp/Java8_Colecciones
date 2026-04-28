b6d38db62d59f083da2d9ad7e4d96697bffc1051
Juli4ndvlp <juliandvlp01@gmail.com>
2026-04-28 14:28:02 -0500

    2nd commit

---
+++ Colecciones/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> tareas = new ArrayList<>(); // crea la lista vacia, y solo recibe strings
+
+        tareas.add("Ver el partido");
+        tareas.add("Estudiar java");
+        tareas.add("Revisar arrays");
+        tareas.add("Partido a las 6");
+    }
+}
